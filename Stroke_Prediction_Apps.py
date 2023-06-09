8fda35a810be27cd6efb3f49c64d1275012c8798
Peilin-CodeVersion <130911172+Peilin-CodeVersion@users.noreply.github.com>
2023-06-09 22:55:13 +0800

    Update Stroke_Prediction_Apps.py

--- Stroke_Prediction_Apps.py
+++ Stroke_Prediction_Apps.py
@@ -31,38 +31,40 @@ def main():
     bmi = st.number_input("BMI")
     smoking_status = st.selectbox("Smoking Status", ("Unknown", "formerly smoked", "never smoked", "smokes"))
 
-    # Save the user inputs into a dataset
-    user_data = pd.DataFrame({
-        "gender": [gender],
-        "age": [age],
-        "hypertension": [hypertension],
-        "heart_disease": [heart_disease],
-        "ever_married": [ever_married],
-        "work_type": [work_type],
-        "Residence_type": [Residence_type],
-        "avg_glucose_level": [avg_glucose_level],
-        "bmi": [bmi],
-        "smoking_status": [smoking_status]
-    })
+    # Create a button to start the prediction
+    if st.button("Predict Stroke Risk"):
+        # Save the user inputs into a dataset
+        user_data = pd.DataFrame({
+            "gender": [gender],
+            "age": [age],
+            "hypertension": [hypertension],
+            "heart_disease": [heart_disease],
+            "ever_married": [ever_married],
+            "work_type": [work_type],
+            "Residence_type": [Residence_type],
+            "avg_glucose_level": [avg_glucose_level],
+            "bmi": [bmi],
+            "smoking_status": [smoking_status]
+        })
 
-    # Encode the user dataset
-    user_data["gender"] = user_data["gender"].map({"Female": 0, "Male": 1}).astype(int)
-    user_data["hypertension"] = user_data["hypertension"].map({"No": 0, "Yes": 1}).astype(int)
-    user_data["heart_disease"] = user_data["heart_disease"].map({"No": 0, "Yes": 1}).astype(int)
-    user_data["ever_married"] = user_data["ever_married"].map({"No": 0, "Yes": 1}).astype(int)
-    user_data["work_type"] = user_data["work_type"].map({"Govt_job": 0, "Never_worked": 1, "Private": 2, "Self-employed": 3, "children": 4}).astype(int)
-    user_data["Residence_type"] = user_data["Residence_type"].map({"Rural": 0, "Urban": 1}).astype(int)
-    user_data["smoking_status"] = user_data["smoking_status"].map({"Unknown": 0, "formerly smoked": 1, "never smoked": 2, "smokes": 3}).astype(int)
+        # Encode the user dataset
+        user_data["gender"] = user_data["gender"].map({"Female": 0, "Male": 1}).astype(int)
+        user_data["hypertension"] = user_data["hypertension"].map({"No": 0, "Yes": 1}).astype(int)
+        user_data["heart_disease"] = user_data["heart_disease"].map({"No": 0, "Yes": 1}).astype(int)
+        user_data["ever_married"] = user_data["ever_married"].map({"No": 0, "Yes": 1}).astype(int)
+        user_data["work_type"] = user_data["work_type"].map({"Govt_job": 0, "Never_worked": 1, "Private": 2, "Self-employed": 3, "children": 4}).astype(int)
+        user_data["Residence_type"] = user_data["Residence_type"].map({"Rural": 0, "Urban": 1}).astype(int)
+        user_data["smoking_status"] = user_data["smoking_status"].map({"Unknown": 0, "formerly smoked": 1, "never smoked": 2, "smokes": 3}).astype(int)
 
-    # Use the dataset to predict the stroke using the trained SVM model
-    stroke_prediction = model.predict(user_data)
+        # Use the dataset to predict the stroke using the trained SVM model
+        stroke_prediction = model.predict(user_data)
 
-    # Show the risk of getting a stroke
-    st.subheader("Stroke Risk Prediction Result:")
-    if stroke_prediction[0] == 0:
-        st.write("Low stroke risk")
-    else:
-        st.write("High stroke risk")
+        # Show the risk of getting a stroke
+        st.subheader("Stroke Risk Prediction Result:")
+        if stroke_prediction[0] == 0:
+            st.write("Low stroke risk")
+        else:
+            st.write("High stroke risk")
 
 if __name__ == "__main__":
     main()
